9bcddabf12d9d96e360ac8ab9dc773f1bef0da7e
Matt Hartfield <matt.hartfield@wwt.com>
2026-05-26 09:53:24 -0500

    Default port to nyc zip

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   base: '/wee-word/',
+  server: {
+    port: 10001,
+  },
   test: {
     environment: 'node',
     include: ['src/tests/**/*.test.js'],
